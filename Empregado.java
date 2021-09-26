e685a2c2caafbaa294422da9cc3009dc0c1362f0
unknown <enoquezh@gmail.com>
2021-09-26 10:38:13 -0300

    Calculo de salario efetuado de acordo com o tipo

--- Empregado.java
+++ Empregado.java
@@ -1,6 +1,24 @@
 public class Empregado{
     String nome;
     int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+
+    public double calculaSalario(){
+        if (tipo ==1){
+            return salario;
+        }
+        else if (tipo ==2){
+            return salario + salario * comissao;
+        }
+        else if (tipo ==3){
+            return salario +bonus;
+        }
+        return 0;
+    }
+
     //camal case
     public void setNome (String nome){
             this.nome = nome;
