cb2b6b2846418b8f7b06deddec5dc4bef8d15010
unknown <enoquezh@gmail.com>
2021-09-26 10:25:26 -0300

    Commit inicial. Classe Empregado criada

---
+++ Empregado.java
@@ -0,0 +1,24 @@
+public class Empregado{
+    String nome;
+    int idade;
+    //camal case
+    public void setNome (String nome){
+            this.nome = nome;
+    }
+
+    public String getNome (){
+        return this.nome;
+    }
+
+    public void setIdadE (int idade){
+            this.idade = idade;
+        }
+
+    public int getIdade (){
+        return this.idade;
+    }
+}
+// untracked  git add -> staged
+// unmodified
+// modified
+// Staged
